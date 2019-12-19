07ae51d4256d03e52a1351feb2e01ed8574f1959
Klvst3r <klvst3r@gmail.com>
2019-12-19 00:51:37 -0600

    2.1. Insert Service

--- inc/conn.php
+++ inc/conn.php
@@ -6,4 +6,10 @@ $pass_BD = "desarrollo";
 $name_BD = "id11991505_phon3gap";
 
 $link = new mysqli($host_BD, $user_BD, $pass_BD, $name_BD);
+
+// if($link){
+// 	echo "Conexion Exitosa";
+// }else{
+// 	echo "Error en la conexion con la BD";
+// }
 ?>
